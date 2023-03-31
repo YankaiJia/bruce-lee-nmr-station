50f0b3688d1794f42cb2bee0269e0db5c27ff894
Yaroslav I. Sobolev <yaroslav.sobolev@gmail.com>
2023-03-31 21:40:58 +0900

    Refactor and add docstrings

--- visualize-results/visualize_results.py
+++ visualize-results/visualize_results.py
@@ -7,6 +7,27 @@ from savitzky_golay_werrors import savgol_filter_werror
 data_folder = os.environ['ROBOCHEM_DATA_PATH'].replace('\\', '/') + '/'
 
 def filter_layouts(array, threshold, verbose=True):
+    """
+    Points that are further away from the previous and next point than the threshold
+    are replaced by the mean of the previous and the next point.
+
+    Parameters
+    ----------
+    array: ndarray
+        1D array of values to be filtered.
+
+    threshold: float
+        The threshold for filtering.
+
+    verbose: bool
+        Whether to print out which points were filtered out.
+
+    Returns
+    -------
+    array: np.array
+        The filtered array.
+
+    """
     diff = np.diff(array)
     for i, x in enumerate(array):
         if i == 0 or i == len(array) - 1:
@@ -21,8 +42,32 @@ def filter_layouts(array, threshold, verbose=True):
     return array
 
 
-# take care of rounding errors
 def round_to_nearest(df_new, df_reference, column_names):
+    """
+    Round the values in the new dataframe to the nearest values in the reference dataframe.
+    For example, if column in reference dataframe has values [0.1, 0.2, 0.3] and the new dataframe has values
+    [5, 8, 11, 0.099999999999999999998, 11, 0.2000000000000000001, 0.35], the new dataframe values will be rounded to
+    [5, 8, 11, 0.1, 0.2, 0.3. 11. 0.2, 0.35]
+    and returned.
+
+    Parameters
+    ----------
+    df_new: pd.DataFrame
+        The dataframe with the new values that may be slightly different from the reference values due to rounding
+        errors.
+
+    df_reference: pd.DataFrame
+        The dataframe with the reference values.
+
+    column_names
+        Only these columns will be changed in the new dataframe. Columns of same names are compared between
+        the dataframes.
+
+    Returns
+    -------
+    df_new: pd.DataFrame
+        The dataframe with the new values rounded to the nearest values in the reference dataframe.
+    """
     for column_name in column_names:
         new_values = df_new[column_name].to_numpy()
         unique_values_from_reference_df = df_reference[column_name].unique()
@@ -34,11 +79,29 @@ def round_to_nearest(df_new, df_reference, column_names):
     return df_new
 
 
-def join_data_from_runs(experiment_names, round_on_columns=('ic001', 'am001', 'ald001', 'ptsa')):
-    df_result = pd.read_csv(data_folder + experiment_names[0] + f'results/product_concentration.csv')
+def join_data_from_runs(run_names, round_on_columns=('ic001', 'am001', 'ald001', 'ptsa')):
+    """
+    Loads the `results/product_concentration.csv` from multiple runs and joins them into one dataframe.
+
+    Parameters
+    ----------
+    run_names: list
+        The names of the runs whose results will be loaded and joined. These names are the names of the subfolders
+        of the data folder.
+
+    round_on_columns
+        The columns that will be rounded to the nearest values in the reference dataframe. This is done to avoid
+        rounding errors when joining the dataframes.
+
+    Returns
+    -------
+    df_result: pd.DataFrame
+        The dataframe with the joined data. The sequence of the rows is the same as the sequence of the runs.
+    """
+    df_result = pd.read_csv(data_folder + run_names[0] + f'results/product_concentration.csv')
     df_result.drop('Unnamed: 0', inplace=True, axis=1)
-    for experiment_name in experiment_names[1:]:
-        df_temporary = pd.read_csv(data_folder + experiment_name + f'results/product_concentration.csv')
+    for run_name in run_names[1:]:
+        df_temporary = pd.read_csv(data_folder + run_name + f'results/product_concentration.csv')
         df_temporary.drop('Unnamed: 0', inplace=True, axis=1)
         df_temporary = round_to_nearest(df_temporary, df_result, round_on_columns)
         df_result = df_result.append(df_temporary, ignore_index=True)
@@ -47,6 +110,46 @@ def join_data_from_runs(experiment_names, round_on_columns=('ic001', 'am001', 'a
 
 def make_spline(xs, ys, yerr, layout_threshold=4, more_error_savgol=0.4, more_error_spline=0.2,
                 spline_smoothing_factor=6, do_plot=False):
+    """
+    Filts spline to the data. Intended for plots of yield across all the catalyst values for specific
+    combination of concentrations of substrates other than the catalyst.
+
+    It applies the weighted Savitsky-Golay filter to the data, then first the weighted spline to the smoothed data.
+    Weight are determined by the supplied yerr values, which should be the measurement uncertainty of individual
+    datapoints.
+
+    Parameters
+    ----------
+    xs: ndarray
+        1D array of x values. E.g. catalyst concentrations.
+
+    ys: ndarray
+        1D array of y values to which the spline will be fitted. E.g.: reaction yields.
+
+    yerr: ndarray
+        The errors of the y values.
+
+    layout_threshold: float
+        The threshold for filtering the data for layouts. See the filter_layouts function above.
+
+    more_error_savgol: float
+        The additional error to be added to the yerr when smoothing the data with weighted Savitsky-Golay filter.
+
+    more_error_spline: float
+        The additional error to be added to the yerr when smoothing the data with weighted spline applied after
+        Savitsky-Golay filter.
+
+    spline_smoothing_factor:
+        The smoothing factor for the spline. See the factor s in the scipy.interpolate.splrep function.
+
+    do_plot: bool
+        If True, the smoothed data will be plotted.
+
+    Returns
+    -------
+        tck: tuple
+            The spline parameters. See the scipy.interpolate.splrep function.
+    """
     ys = filter_layouts(ys, threshold=layout_threshold)
     ys_sg = savgol_filter_werror(ys, window_length=5, degree=2, error=yerr + more_error_savgol)
     if do_plot:
@@ -59,8 +162,50 @@ def plot_one_point_across_catalyst_range(df_data, param_values_by_index, label=N
                                          column_names=('ic001', 'am001', 'ald001'),
                                          catalyst_name='ptsa', column_to_plot='yield', relative_std=0.146,
                                          withspline=True):
+    """
+    Plots data across the catalyst range for a specific combination of non-catalyst substrate concentrations
+    (and perhaps other parameters) defined by the indices in lists if unique values of the parameters.
+
+    For example, if param_values_by_data=(2, 0, 1) and column_names=('ic001', 'am001', 'ald001')
+    then the function will plot the data from the dataset where the column `ic001` has the third unique value
+    from the sorted list of unique values of column `ic001`, the column `am001` has the first unique value, and
+    the column `ald001` has the second unique value.
+
+    Parameters
+    ----------
+    df_data: pd.DataFrame
+        The dataframe with the data to be plotted. The dataframe should have the columns catalyst_name,
+        column_to_plot, and all the columns in `column_names` list (or tuple).
+
+    param_values_by_index: tuple
+        The indices of the unique values of the parameters that will be used to select the data to be plotted.
+
+    label: str
+        The label of the plot. If None, the label will be the values of the parameters that were used to select
+        the data to be plotted.
+
+    color: str
+        The color of the plot. If None, the color will be the default color of the plot.
+
+    column_names: tuple
+        The names of the columns that will be used to select the data to be plotted. The length of the list
+        should be the same as the length of the `param_values_by_index` list.
+
+    catalyst_name: str
+        The name of the column that contains the catalyst concentrations.
+
+    column_to_plot:
+        The name of the column that contains the data to be plotted. For example, the yield of the reaction.
+
+    relative_std:
+        The relative standard deviation of the data to be plotted. This is used to determine the error bars.
+
+    withspline:
+        If True, the spline will be plotted on top of the data. See the make_spline function above.
+    """
     target = {column_name: sorted(df_data[column_name].unique())[param_values_by_index[i]]
               for i, column_name in enumerate(column_names)}
+
     indices = df_data[(df_data[column_names[0]] == target[column_names[0]]) &
                       (df_data[column_names[1]] == target[column_names[1]]) &
                       (df_data[column_names[2]] == target[column_names[2]])].index
@@ -69,6 +214,10 @@ def plot_one_point_across_catalyst_range(df_data, param_values_by_index, label=N
     xs = df_data[catalyst_name].to_numpy()[indices]
     ys = df_data[column_to_plot].to_numpy()[indices] * scale_factor
     yerr = df_data[column_to_plot].to_numpy()[indices] * scale_factor * relative_std
+
+    if label is None:
+        label = f'{column_names}: {param_values_by_index}'
+
     if color is None:
         plt.errorbar(xs, ys, yerr=yerr, linestyle='None', marker='o', capsize=3, label=label, alpha=0.5)
     else:
