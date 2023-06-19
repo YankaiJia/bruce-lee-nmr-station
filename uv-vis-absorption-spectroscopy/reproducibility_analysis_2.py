fa3ed6dcf99fffd20c7d83203c72081e0cdc8489
Yaroslav I. Sobolev <yaroslav.sobolev@gmail.com>
2023-06-19 23:00:54 +0900

    Generate list of conditions for `multicomp-reactions/2023-06-19-run01`.
    In addition to `multicomp_generate_C.py` and `multicomp_C_to_V.py` that do generation of concentration list and the conversion from concentrations to volumes, respectively, I modified some files to view the datacube with mock product concentrations.

--- uv-vis-absorption-spectroscopy/reproducibility_analysis_2.py
+++ uv-vis-absorption-spectroscopy/reproducibility_analysis_2.py
@@ -94,7 +94,8 @@ def print_stats(input_array, label):
     return np.std(input_array) / np.mean(input_array)
 
 
-def view_reproducibility_for_one_plate(plate_name, run_name, with_dilution=True, title=None, force_recaltulation=False):
+def view_reproducibility_for_one_plate(plate_name, run_name, with_dilution=True, title=None, force_recaltulation=False,
+                                       correction_factor=1):
     """
     View the reproducibility of the concentrations of product RF029A for one plate. It makes two plots:
     1. The concentrations vs. vial ID.
@@ -131,26 +132,71 @@ def view_reproducibility_for_one_plate(plate_name, run_name, with_dilution=True,
     else:
         concentrations = process_or_load_processed(nickname=plate_name, plate_name=plate_name, run_name=run_name,
                                                    force_recalculation=force_recaltulation)
+    concentrations = correction_factor * concentrations
     rel_std = print_stats(concentrations, label=title)
-    plt.figure(1)
-    plt.scatter(np.arange(concentrations.shape[0]), concentrations, label=title)
-    plt.title(f'{title}. Relative error {rel_std * 100:.1f}%')
-    plt.xlabel('Vial ID')
-    plt.ylabel('Product concentration in mol/L')
-    plt.ylim(0, max(concentrations) * 1.4)
-    plt.legend()
+    fig, [ax1, ax2] = plt.subplots(2)
+    ax1.scatter(np.arange(concentrations.shape[0]), concentrations, label=title)
+    ax1.set_title(f'{title}. Relative error {rel_std * 100:.1f}%')
+    ax1.set_xlabel('Vial ID')
+    ax1.set_ylabel('Product concentration in mol/L')
+    ax1.set_ylim(0, max(concentrations) * 1.4)
+    ax1.legend()
 
     if with_dilution:
         as_plate = concentrations.reshape((3, 9))
     else:
         as_plate = concentrations.reshape((6, 9))
-    plt.figure(2)
-    plt.imshow(as_plate)
-    plt.title(title)
+    # plt.figure(2)
+    ax2.imshow(as_plate)
+    # plt.title(title)
+    plt.tight_layout()
+    fig.savefig(data_folder + run_name + 'results/validation/' + f'{title}.png', dpi=300)
     plt.show()
+
+    return concentrations
 
 
 if __name__ == '__main__':
-    view_reproducibility_for_one_plate(plate_name='2023-06-15_17-08-49__plate0000039__one-reaction-2023-05-14-run01',
-                                       run_name='multicomp-reactions/2023-06-14-run02-dmf/',
-                                       title='2023-06-14-run01. Plate 39. Same reaction in all vials')
+    # view_reproducibility_for_one_plate(plate_name='2023-06-15_17-08-49__plate0000039__one-reaction-2023-05-14-run01',
+    #                                    run_name='multicomp-reactions/2023-06-14-run02-dmf/',
+    #                                    title='2023-06-14-run01. Plate 39. Same reaction in all vials')
+
+    # view_reproducibility_for_one_plate(plate_name='2023-06-16_20-27-22__plate0000042__multicomp-51room_light_35blacked',
+    #                                    run_name='multicomp-reactions/2023-03-20-run01/',
+    #                                    title='multicomp-51room_light_35blacked')
+
+    # conc = view_reproducibility_for_one_plate(plate_name='2023-06-15_19-32-20__plate0000041__multicomp-2023-05-15-run01-calibration',
+    #                                    run_name='multicomp-reactions/2023-06-15-run01/',
+    #                                    title='recalibration',
+    #                                    with_dilution=False)
+    # print(conc)
+    # Conclusion: it shows that the concentrations are now being underestimated by 1.56 times
+
+    # view_reproducibility_for_one_plate(plate_name='2023-06-16_15-14-07__plate0000040__multicomp-2023-06-15-run01-repeatability_9-11_corrupted',
+    #                                    run_name='multicomp-reactions/2023-06-15-run02/',
+    #                                    title='N2_PA12_9-11-corrupted',
+    #                                    correction_factor=1.56)
+    # view_reproducibility_for_one_plate(plate_name='2023-06-16_15-58-53__plate0000035__multicomp-2023-06-15-run01-repeatability_N2_AL',
+    #                                    run_name='multicomp-reactions/2023-06-15-run02/',
+    #                                    title='N2_AL',
+    #                                    correction_factor=1.56)
+    # view_reproducibility_for_one_plate(plate_name='2023-06-16_16-36-57__plate0000036__multicomp-2023-06-15-run01-repeatability_Air_PA12',
+    #                                    run_name='multicomp-reactions/2023-06-15-run02/',
+    #                                    title='Air_PA12',
+    #                                    correction_factor=1.56)
+    # view_reproducibility_for_one_plate(plate_name='2023-06-16_16-51-31__plate0000046__multicomp-2023-06-15-run01-repeatability_Air_AL',
+    #                                    run_name='multicomp-reactions/2023-06-15-run02/',
+    #                                    title='Air_AL',
+    #                                    correction_factor=1.56)
+
+    # view_reproducibility_for_one_plate(plate_name='2023-06-17_18-00-02__plate0000046__multicomp_reactions_2023-06-16-run01-reproduction',
+    #                                    run_name='multicomp-reactions/2023-06-16-run01/',
+    #                                    title='NW_AL_2023-06-17_noextrapolation',
+    #                                    correction_factor=1,
+    #                                    force_recaltulation=True)
+
+    view_reproducibility_for_one_plate(plate_name='2023-06-13_11-13-01__plate0000037__multicomponent-reactions-2023-06-13-new',
+                                       run_name='multicomp-reactions/2023-06-13-run01/',
+                                       title='2023-06-13-new',
+                                       correction_factor=1,
+                                       force_recaltulation=True)
