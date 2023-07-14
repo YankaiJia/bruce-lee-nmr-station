c178708189c5f99cc0eef158b5f6d70069461850
Yaroslav I. Sobolev <yaroslav.sobolev@gmail.com>
2023-07-14 22:28:25 +0900

    Create tests for `load_df_from_dilution_info` and `load_df_from_run_info`
    Test copies all structure from `tests/test_organize_run_results` directory into a temporary directory, which is later treated as the data_folder (that is normally in the Dropbox, but not for tests). Test loads the run_info and dilution_info from respective locations in the `multicomp-reactions/2023-06-20-run01/` in the temporary folder and then checks the results against dataframe loaded from `expected_outputs/dilution_info.pkl` and `expected_outputs/run_info.pkl`.

---
+++ tests/test_organize_run_results.py
@@ -0,0 +1,62 @@
+import importlib
+from distutils import dir_util
+import pandas as pd
+from pytest import fixture
+import os
+
+organize_run_results = importlib.import_module("misc-scripts.organize_run_results")
+
+
+@fixture
+def datadir(tmpdir, request):
+    '''
+    Fixture responsible for searching a folder with the same name of test module and, if available, moving all
+    contents to a temporary directory so tests can use them freely.
+    '''
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+
+    return tmpdir
+
+
+def test_load_df_from_run_info(datadir):
+    """
+    Test uses a fixture that copies all structure from `tests/test_organize_run_results` directory into a temporary
+    directory, which is later treated as the data_folder (that is normally in the Dropbox, but not for tests). Test
+    loads the run_info from respective locations in the `multicomp-reactions/2023-06-20-run01/` in the temporary
+    folder and then checks the results against an expected dataframe loaded from `expected_outputs/run_info.pkl`.
+
+    Parameters
+    ----------
+    datadir: pytest fixture
+        Temporary directory with the same structure as `tests/test_organize_run_results` directory.
+    """
+    data_folder = ''
+    with datadir.as_cwd():
+        experiment_name = 'multicomp-reactions/2023-06-20-run01/'
+        df_loaded = organize_run_results.load_df_from_run_info(data_folder + experiment_name + 'pipetter_io/run_info.csv')
+        df_expected = pd.read_pickle('expected_outputs/run_info.pkl')
+        pd.testing.assert_frame_equal(df_loaded, df_expected)
+
+
+def test_load_df_from_dilution_info(datadir):
+    """
+    Test uses a fixture that copies all structure from `tests/test_organize_run_results` directory into a temporary
+    directory, which is later treated as the data_folder (that is normally in the Dropbox, but not for tests). Test
+    loads the dataframw eith information about dilution from respective locations in the
+    `multicomp-reactions/2023-06-20-run01/` in the temporary folder and then checks the results against an expected
+    dataframe loaded from `expected_outputs/dilution_info.pkl`.
+
+    Parameters
+    ----------
+    datadir: pytest fixture
+        Temporary directory with the same structure as `tests/test_organize_run_results` directory.
+    """
+    data_folder = ''
+    craic_folder = data_folder + 'craic_microspectrometer_measurements/absorbance/'
+    with datadir.as_cwd():
+        pd.testing.assert_frame_equal(organize_run_results.load_df_from_dilution_info('multicomp-reactions/2023-06-20-run01/'),
+                                      pd.read_pickle('expected_outputs/dilution_info.pkl'))
