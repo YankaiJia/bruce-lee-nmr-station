b9c59eb894d7975c4ec3998cd936a309fd9ae6ef
Yankai_RoboRea#2 <jiayankaieric@gmail.com>
2023-11-28 12:12:36 +0900

    add arduino code

---
+++ zeus-pipetter/arduino_code/lid_open_close/lid_open_close.ino
@@ -0,0 +1,73 @@
+#include <Servo.h>
+Servo servo;
+int angle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(25);
+  
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  pinMode(3, OUTPUT); 
+  digitalWrite(3, HIGH);
+  pinMode(4, OUTPUT); 
+  digitalWrite(4, HIGH);
+
+  servo.attach(9);
+  servo.write(angle);  
+}
+
+void close();
+void open();
+
+void loop()
+{
+while (Serial.available() == 0) {}
+String user_input = Serial.readString();
+
+user_input.trim();
+//Serial.println(user_input);
+if(user_input == "1"){
+  //Serial.println("1");
+  open();
+}
+else if(user_input == "0"){
+  //Serial.println("0");
+  close();
+}
+else if(user_input == "21"){
+  digitalWrite(2, HIGH);
+  }
+else if(user_input == "20"){
+  digitalWrite(2, LOW);
+  }
+ else if(user_input == "31"){
+  digitalWrite(3, HIGH);
+  }
+else if(user_input == "30"){
+  digitalWrite(3, LOW);
+  }
+else if(user_input == "41"){
+  //Serial.println("41");
+  digitalWrite(4, HIGH);
+  }
+else if(user_input == "40"){
+  //Serial.println("40");
+  digitalWrite(4, LOW);
+  }
+}
+
+void close()
+{
+for(angle = 90; angle > 3; angle-=2)    
+{                                
+  servo.write(angle);           
+  delay(20); 
+}
+}
+
+void open()
+{
+servo.write(90);
+delay(500);
+}
