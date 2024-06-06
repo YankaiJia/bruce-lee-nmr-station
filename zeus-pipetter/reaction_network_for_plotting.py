bc65b60f35ac19ae6e6871a9ff588599aabf859e
yankai_jia <jiayankaieric@gmail.com>
2024-06-06 18:48:24 +0900

    office_pc_version

--- zeus-pipetter/reaction_network_for_plotting.py
+++ zeus-pipetter/reaction_network_for_plotting.py
@@ -1,3 +1,5 @@
+import json,os
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -7,8 +9,69 @@ from pathlib import Path
 import math
 from datetime import datetime
 
+from plotly.subplots import make_subplots
+
 matplotlib.use('Agg')
 import plotly.graph_objects as go
+
+
+def plot_scatter():
+
+    fig = px.scatter_3d(df_rxn, x='a_init', y='b_init', z='c_init',
+                        color='abc_yield', size='abc_yield', size_max=18, opacity=0.7)
+
+    # tight layout
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+    return fig
+
+
+def plot_isosurface(df, ks, reaction_product):
+
+    X = df['a_init']
+    Y = df['b_init']
+    Z = df['c_init']
+
+    fig = make_subplots(rows=1, cols=2,subplot_titles=(f'Yield_{reaction_product}',  f'Conc_{reaction_product}'),
+                        specs=[[{'type': 'Isosurface'}, {'type': 'Isosurface'}]])
+
+    plot1 = go.Isosurface(
+        x=X,
+        y=Y,
+        z=Z,
+        value=df[f'{reaction_product}_yield'],
+        isomin=min(df[f'{reaction_product}_yield']),
+        isomax=max(df[f'{reaction_product}_yield']),
+        surface_count=8, colorscale='Rainbow',colorbar=dict(len=0.5, x=0.45 ,y=0.5),
+        caps=dict(x_show=False, y_show=False), text=f'{reaction_product}_yield'
+    )
+    plot2 = go.Isosurface(
+        x=X,
+        y=Y,
+        z=Z,
+        value=df[f'{reaction_product}_final'],
+        isomin=min(df[f'{reaction_product}_final']),
+        isomax=max(df[f'{reaction_product}_final']),
+        surface_count=8, colorscale='Rainbow',colorbar=dict(len=0.5, x=1.0 ,y=0.5),
+        caps=dict(x_show=False, y_show=False), text=f'{reaction_product}_final'
+
+    )
+
+    fig.add_trace(plot1, 1, 1)
+    fig.add_trace(plot2, 1, 2)
+
+    fig.update_layout(scene=dict(
+        xaxis_title='conc_A0',
+        yaxis_title='conc_B0',
+        zaxis_title='conc_C0'),
+        width=1600,
+        margin=dict(r=80, b=80, l=80, t=100),
+        title= f"k={ks}",
+        title_font_size = 30,)
+
+    fig.update_annotations(font_size=30)
+
+    return fig
 
 def plot_concs(a_list, b_list, c_list, ab_list, bc_list, ac_list, abc_list, a0, b0, c0):
     t_step_size = 0.02
@@ -35,158 +98,37 @@ def plot_concs(a_list, b_list, c_list, ab_list, bc_list, ac_list, abc_list, a0, 
 
     return fig
 
-def kinetic_equations(a, b, c, ab, bc, ac, kab, kac, kbc, kabc, kacb, kbca):
-
-    d_a = -kab * a * b - kac * a * c - kbca * bc * a
-    d_b = -kab * a * b - kbc * b * c - kacb * ac * b
-    d_c = -kac * a * c - kbc * b * c - kabc * ab * c
-    d_ab = kab * a * b - kabc * ab * c
-    d_bc = kbc * b * c - kbca * bc * a
-    d_ac = kac * a * c - kacb * ac * b
-    d_abc = kabc * ab * c + kbca * bc * a + kacb * ac * b
-
-    return d_a, d_b, d_c, d_ab, d_bc, d_ac, d_abc
-
-def concentration_iterate(a:float=1, b:float=1, c:float=1,
-                          kab:float=1, kac:float=1, kbc:float=1,
-                          kabc:float=1, kacb:float=1, kbca:float=1,
-                          t_step_size:float=0.02, t_end:int=50000):
-
-    a_list, b_list, c_list, ab_list, bc_list, ac_list, abc_list = [], [], [], [], [], [], []
-    ab, bc, ac, abc = 0, 0, 0, 0
-
-    num = 0
-    while num < t_end:
-        d_a, d_b, d_c, d_ab, d_bc, d_ac, d_abc = (
-            kinetic_equations(a, b, c, ab, bc, ac, kab, kac, kbc, kabc, kacb, kbca))
-        a += d_a*t_step_size
-        b += d_b*t_step_size
-        c += d_c*t_step_size
-        ab += d_ab*t_step_size
-        bc += d_bc*t_step_size
-        ac += d_ac*t_step_size
-        abc += d_abc*t_step_size
-
-        a_list.append(a)
-        b_list.append(b)
-        c_list.append(c)
-        ab_list.append(ab)
-        bc_list.append(bc)
-        ac_list.append(ac)
-        abc_list.append(abc)
-
-        num += 1
-
-        if num > 100 and math.isclose(abc_list[-1],abc_list[-2],  rel_tol=1e-07):
-            print(f"conc_abc converged after {num} steps at {abc_list[-1]} and {abc_list[-2]}.")
-            break
-
-    return a_list, b_list, c_list, ab_list, bc_list, ac_list, abc_list
-
-
-def sweep_diff_concs(path:str,c_start:float,c_stop:float,c_num:int,
-                     kab:float, kac:float, kbc:float,
-                     kabc:float, kacb:float, kbca:float):
-
-    df = pd.DataFrame(columns=["a_init", "b_init", "c_init", "abc_final", "abc_yield"])    # set column names as "a_init", "b_init", "c_init"
-
-    index = 0
-    for a0 in np.linspace(c_start, c_stop,c_num, endpoint= False):
-        for b0 in np.linspace(c_start, c_stop,c_num, endpoint= False):
-            for c0 in np.linspace(c_start, c_stop,c_num, endpoint= False):
-
-                a_list, b_list, c_list, ab_list, bc_list, ac_list, abc_list\
-                                            = concentration_iterate(a=a0, b=b0, c=c0,
-                                            kab=kab, kac=kac, kbc=kbc,
-                                            kabc=kabc, kacb=kacb, kbca=kbca)
-                if index % 100 == 0:
-                    print(len(abc_list),'saved!')
-                    fig_kinetics = plot_concs(a_list, b_list, c_list,
-                                              ab_list, bc_list, ac_list, abc_list,
-                                              a0,b0,c0)
-                    Path(path + '/kinetics_2d_plots').mkdir(parents=True, exist_ok=True)
-                    fig_kinetics.savefig(path + '/kinetics_2d_plots/' + f'a0_{round(a0,2)}_b0_{round(b0,2)}_c0_{round(c0,2)}.png')
-                    plt.close(fig_kinetics)
-
-                abc_final = abc_list[-1]
-                abc_list.append(abc_final)
-
-                if abc_final == 0:
-                    abc_yield = 0
-                else:
-                    abc_yield = abc_final / min(a0, b0, c0)
-                df.loc[index] = [a0, b0, c0, abc_final, abc_yield]
-
-                index += 1
-
-                print(index)
-
-
-    return df
-
-def plot_scatter():
-
-    fig = px.scatter_3d(df_rxn, x='a_init', y='b_init', z='c_init',
-                        color='abc_yield', size='abc_yield', size_max=18, opacity=0.7)
-
-    # tight layout
-    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-
-    return fig
-
-def plot_isosurface(df, ks):
-
-    X = df['a_init']
-    Y = df['b_init']
-    Z = df['c_init']
-
-    abc_final = df['abc_final']
-    abc_yield = df['abc_yield']
-
-    fig = go.Figure(data=go.Isosurface(
-        x=X,
-        y=Y,
-        z=Z,
-        value=abc_final,
-        isomin=min(abc_final) ,
-        isomax=max(abc_final) ,
-        surface_count=8,
-        opacity=1,
-        caps=dict(x_show=False, y_show=False),
-        colorscale="Rainbow"
-    ))
-
-    fig.update_layout(scene=dict(
-        xaxis_title='conc_A0',
-        yaxis_title='conc_B0',
-        zaxis_title='conc_C0'),
-        width=1200,
-        margin=dict(r=50, b=50, l=50, t=100),
-        title= f"k=({ks[0]}, {ks[1]}, {ks[2]}, {ks[3]}, {ks[4]}, {ks[5]})",
-        title_font_size = 30)
-
-    return fig
-
 
 if __name__ == "__main__":
 
-    # data_dir = ('reaction_network_simulation\\k_0.1_0.1_0.1_1_1_1\\')
-    # data_dir = ('reaction_network_simulation\\k_1_0.1_0.1_1_0.1_0.1\\')
-    # data_dir = ('reaction_network_simulation\\k_1_1_0.1_1_1_0.1\\')
-    data_dir = ('reaction_network_simulation\\k_1_1_1_0.1_0.1_0.1\\')
-    # data_dir = ('reaction_network_simulation\\k_1_1_1_1_1_1\\')
 
-    time_now = datetime.today().strftime('%Y-%m-%d-%H-%M')
+    data_dir = 'reaction_network_simulation_systematic_cal_wo_reverse'
 
-    df_rxn = pd.read_csv(data_dir + data_dir.split('\\')[-2] + '.csv')
+    # get all the subfolder in the data_dir
+    data_dir = Path(data_dir)
+    subfolders = [f for f in data_dir.glob('*') if f.is_dir()]
 
-    ks = [float(i) for i in data_dir.split('\\')[-2].split('_')[1:]]
+    for subfolder in subfolders:
+        # get all the csv files in the subfolder
+        csv_files = [f for f in subfolder.glob('*.csv')]
+        print(subfolder)
 
-    # fig_scatter = plot_scatter()
-    # fig_scatter.show()
-    fig_isosurface = plot_isosurface(df=df_rxn, ks=(ks[0],ks[1],ks[2],ks[3],ks[4],ks[5]))
-    fig_isosurface.write_html(data_dir + f'k_{ks[0]}_{ks[1]}_{ks[2]}_{ks[3]}_{ks[4]}_{ks[5]}_{time_now}.html')
-    fig_isosurface.show()
+        for csv_file in csv_files:
+            # rename the json file to the same name as the csv file
+            json_file=csv_file.with_suffix('.json')
+            # read the json file
+            with open(json_file) as f:
+                data = json.load(f)
+
+            ks = data['ks']
+            df_rxn = pd.read_csv(csv_file)
+            # fig_scatter = plot_scatter()
+            # fig_scatter.show()
+            ## reduce the number of points to plot by 2
+            df_rxn = df_rxn.iloc[::5, :]
+            fig_isosurface = plot_isosurface(df=df_rxn, ks=(ks[0],ks[1],ks[2],ks[3],ks[4],ks[5]))
+            fig_isosurface.write_html(csv_file.with_suffix('.html'))
+            fig_isosurface.show()
 
 
 
