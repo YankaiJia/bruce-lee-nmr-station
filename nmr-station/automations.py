d84da87def267aaec841d96dfef59f617784c6ac
azidoazideazide <eddykwong5a19@gmail.com>
2024-06-24 17:30:37 +0900

    complete auto route slot2, nmr, waiter, and washer

--- nmr-station/automations.py
+++ nmr-station/automations.py
@@ -1,17 +1,20 @@
+import time
+
+
 from meca import get_robot, connect_robot, config_robot
 
 
-def demo_20June():
+def demo_27June():
    # // Pick the tube from Slot 1
    r.GripperOpen()
 
 
-   r.MoveJoints(77.19999, 7.26299, -12.64184, 0, 5.37785, 0)
-   r.MoveLinRelWrf(0, 0, -242, 0, 0, 0)
+   r.MoveJoints(77.19999, 12.99229, -30.00896, 0, 17.01568, 0)
+   r.MoveLinRelWrf(0, 0, -262, 0, 0, 0)
    r.GripperClose()
    # r.MoveLinRelWrf(0, 0, 242, 0, 0, 0)
    # avoid clashing the waiting slot
-   r.MoveLinRelWrf(0, 0, 272, 0, 0, 0)
+   r.MoveLinRelWrf(0, 0, 262, 0, 0, 0)
 
 
    # // from slot 1 to spinsolve
@@ -48,6 +51,66 @@ def demo_20June():
    r.MoveLinRelWrf(0, 0, 252, 0, 0, 0)
 
 
+   # moving to the washer part
+   r.MoveJoints(33.65, 23.56935, -50.52051, 0, 26.95017, 0)
+   r.MoveLinRelWrf(0, 0, -280, 0, 0, 0)
+   time.sleep(0.5)
+   r.MoveLinRelWrf(0, 0, 280, 0, 0, 0)
+
+
+   # go to waiting part to flip again
+   r.MoveJoints(0, 21.2873, -54.27506, 0, 32.98676, 0)
+   r.MoveLinRelWrf(0, 0, -252, 0, 0, 0)
+   r.GripperOpen()
+   r.MoveLinRelWrf(0, 0, 252, 0, 0, 0)
+
+
+   r.MoveJointsRel(0, 0, 0, 0, 0, 180)
+   r.MoveLinRelWrf(0, 0, -340, 0, 0, 0)
+   r.GripperClose()
+   r.MoveLinRelWrf(0, 0, 340, 0, 0, 0)
+
+
+   # return to slot 1
+   r.MoveJoints(77.19999, 12.99229, -30.00896, 0, 17.01568, 0)
+   r.MoveLinRelWrf(0, 0, -260, 0, 0, 0)
+   r.GripperOpen()
+   r.MoveLinRelWrf(0, 0, 260, 0, 0, 0)
+
+
+def test_washer_needle():
+   r.MoveJoints(33.65, 23.56935, -50.52051, 0, 26.95017, 0)
+   r.MoveLinRelWrf(0, 0, -280, 0, 0, 0)
+   r.GripperOpen()
+   r.GripperClose()
+   r.MoveLinRelWrf(0, 0, 280, 0, 0, 0)
+   time.sleep(5)
+
+
+def test_from_washer_to_slot1():
+   r.MoveJoints(33.65, 23.56935, -50.52051, 0, 26.95017, 0)
+
+
+   # go to waiting part to flip again
+   r.MoveJoints(0, 21.2873, -54.27506, 0, 32.98676, 0)
+   r.MoveLinRelWrf(0, 0, -252, 0, 0, 0)
+   r.GripperOpen()
+   r.MoveLinRelWrf(0, 0, 252, 0, 0, 0)
+
+
+   r.MoveJointsRel(0, 0, 0, 0, 0, 180)
+   r.MoveLinRelWrf(0, 0, -340, 0, 0, 0)
+   r.GripperClose()
+   r.MoveLinRelWrf(0, 0, 340, 0, 0, 0)
+
+
+   # return to slot 1
+   r.MoveJoints(77.19999, 12.99229, -30.00896, 0, 17.01568, 0)
+   r.MoveLinRelWrf(0, 0, -260, 0, 0, 0)
+   r.GripperOpen()
+   r.MoveLinRelWrf(0, 0, 260, 0, 0, 0)
+
+
 
 
 if __name__ == '__main__':
@@ -64,4 +127,8 @@ if __name__ == '__main__':
    # r.SetJointLimits(6, -270, 270)
 
 
-   demo_20June()
+   # demo_20June()
+
+
+   # test_from_washer_to_slot1()
+   demo_27June()
