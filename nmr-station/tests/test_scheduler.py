9291e5cd2e6d8e64a4eb031a1d0f1bed0d2dabcf
azidoazideazide <eddykwong5a19@gmail.com>
2024-07-18 03:03:57 +0900

    write first draft of scheduler and dummy component decision class

--- nmr-station/tests/test_scheduler.py
+++ nmr-station/tests/test_scheduler.py
@@ -5,7 +5,7 @@ import threading
 class SharedState:
     def __init__(self) -> None:
         self.state = {
-            'robot': 'idle',
+            'robot_arm': 'idle',
             'NMR_spectrometer': 'idle',
             'pipetter': 'idle'
         }
@@ -20,13 +20,16 @@ class SharedState:
         with self.lock:
             self.state[facility_name] = latest_state
 
+    def no_message(self) -> bool:
+        return self.message_queue.empty()
+
     def get_front_message(self) -> str :
         return self.message_queue.get()
     
-    def enqueue_message(self, message: str):
+    def add_new_message(self, message: str):
         self.message_queue.put(message)
     
-    def dequeue_message(self):
+    def finish_front_message(self):
         self.message_queue.task_done()
     
 
@@ -44,13 +47,91 @@ class Scheduler:
         threading.Thread(target=self.pipetter.run, args=(self.shared_state,)).start()
 
 
-class RobotArmLogic:
-    pass
+class DummyRobotArmDecision:
+    def __init__(self):
+        pass 
+    
+    def run(self, shared_state: SharedState):
+        while True:
+            my_state = shared_state.get_state()['robot_arm']
+
+            if shared_state.no_message():
+                if my_state == 'idle':
+                    shared_state.add_new_message("NextSample=?")
+                
+                continue
+
+            message = shared_state.get_front_message()
+            if message == "NextSample=1":
+                shared_state.finish_front_message()
+                shared_state.add_new_message("PauseRefill")
+
+            elif message == "PauseRefillOkay":
+                print("robo.move_to(\"tube_rack\")")
+                shared_state.finish_front_message()
+                shared_state.add_new_message("ResumeRefill")
+
+                print("robo.move_to(\"spinsolve80\")")        
+                print("robo.tilted_insert_tube()")
+                print("robo.place_tube(\"spinsolve80\")")
+                shared_state.add_new_message("NewSampleReady")
+
+            elif message == "DitchUsedSample":
+                print("robo.pick_tube(\"spinsolve80\")")
+                print("robo.tilted_remove_tube()")
+
+
+class Dummy_NMR_SpectrometerDecision:
+    def __init__(self) -> None:
+        pass
+
+    def run(self, shared_state: SharedState):
+        while True:
+            my_state = shared_state.get_state()['NMR_spectrometer']
+
+            if my_state == "completed":
+                shared_state.add_new_message("DitchSample")
+                continue
+
+            if shared_state.no_message(): continue
+
+            message = shared_state.get_front_message()
+            if message == "NewSampleReady":
+                print("remote_control.order_new_protocol()")
+                shared_state.finish_front_message()
+
+class DummyPipetterDecision:
+    def __init__(self) -> None:
+        pass
+    
+    def run(self, shared_state: SharedState):
+        while True:
+            my_state = shared_state.get_state()['pipetter']
+            
+            if shared_state.no_message(): continue
+
+            message = shared_state.get_front_message()
+            if message == "NextSample=?":
+                if my_state == "idle":
+                    shared_state.add_new_message("NextSample=None")
+                else:
+                    shared_state.add_new_message("NextSample=(int)")
+                shared_state.finish_front_message()
+            elif message == "PauseRefill":
+                print("pipetter.stand_by()")
+                if my_state == "stand_by":
+                    shared_state.add_new_message("PauseRefillOkay")
+                    shared_state.finish_front_message()
+            elif message == "ResumeRefill":
+                print("pipetter.refill()")
+                shared_state.finish_front_message()
+            
 
 if __name__ == "__main__":
     # testing
-    sample_robot_arm = SampleRobotArm()
-    sample_NMR_spectrometer = Sample_NMR_Spectrometer()
-    sample_pipetter = SamplePipetter()
-    scheduler = Scheduler(sample_robot_arm, sample_NMR_spectrometer, sample_pipetter)
+    dummy_robot_arm = DummyRobotArmDecision()
+    dummy_pipetter = DummyPipetterDecision()
+    dummy_NMR_spectrometer = Dummy_NMR_SpectrometerDecision()
+    # scheduler = Scheduler(sample_robot_arm, sample_NMR_spectrometer, sample_pipetter)
+    scheduler = Scheduler(dummy_robot_arm, dummy_NMR_spectrometer, dummy_pipetter)
     scheduler.start()
