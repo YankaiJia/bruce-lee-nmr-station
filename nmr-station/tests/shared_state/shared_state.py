376da6531d9578eeb3dd47d9504265ec69754cf9
azidoazideazide <eddykwong5a19@gmail.com>
2024-08-04 23:07:00 +0900

    organize scheduler code, create share_state folder

--- nmr-station/tests/shared_state/shared_state.py
+++ nmr-station/tests/shared_state/shared_state.py
@@ -8,19 +8,3 @@ class SharedState:
         self.tube = TubeManager(4)
         self.message_queue = MessageQueue()
         self.lock = threading.Lock()
-
-    def no_message(self) -> bool:
-        with self.lock:
-            return (self.message_queue.qsize() == 0)
-
-    def get_front_message(self) -> str :
-        with self.lock:
-            return list(self.message_queue.queue)[0]
-    
-    def add_new_message(self, message: str):
-        with self.lock:
-            self.message_queue.put(message)
-    
-    def finish_front_message(self):
-        self.message_queue.get()
-        self.message_queue.task_done()
