bf68dff32360f4a992e17ad9ef51e63eb04da431
Yankai <jiayankaieric@gmail.com>
2024-08-28 14:45:45 +0900

    update liquid_level_sensor.ino

--- nmr-station/liquid_level_sensor/liquid_level_sensor.ino
+++ nmr-station/liquid_level_sensor/liquid_level_sensor.ino
@@ -1,66 +1,130 @@
-// name pin 4, 7, 8, 12 as sol_pump_rel1, sol_switch_rel2, air_switch_rel3 and to_be_used_rel4
-int sol_pump_rel1 = 4;
-int sol_switch_rel2 = 7;
-int air_switch_rel3 = 8;
-int to_be_used_rel4 = 12;
-int sol_level_sensor = 6;
+#include <Arduino.h>
 
-int CLEANER_SWITCH = 10; // this will be connected to a physical switch
+//rel, rel2, rel3, rel4 is connected to 4, 7, 8, 12 by hardware.
+const int pump2 = 4; // this is rel1
+const int pump1 = 7; // this is rel2
+const int valve1 = 8; // this is rel3
+const int valve2 = 12; // this is rel4
+const int sensor1 = 5;
+const int sensor2 = 6;
 
-int liquid_sensor_status_current = 0;
-int liquid_sensor_status_previous = 0;
+// main switch, this will be connected to a physical switch
+const int CLEANER_SWITCH = 10;
 
-unsigned long liquid_sensor_trigger_ON_timestamp = 0;
-unsigned long liquid_sensor_trigger_OFF_timestamp = 0;
+unsigned long sensor1_last_changed_time = 0;
+unsigned long sensor2_last_changed_time = 0;
+unsigned long debounceDelay = 500;
+
+bool sensor1_status_last = HIGH; // HIGH means sensor in air
+bool sensor2_status_last = HIGH;
+bool sensor1_status;
+bool sensor2_status;
+bool pump1_status = LOW;
+bool pump2_status = LOW;
+bool valve1_status = HIGH;
+bool valve2_status = HIGH;
 
 void setup() 
 {
   pinMode(CLEANER_SWITCH, INPUT);
-  pinMode(LIQUID_SENSOR, INPUT);
-  pinMode(PUMP_SWITCH, OUTPUT);
+  pinMode(sensor1, INPUT);
+  pinMode(pump1, OUTPUT);
+  pinMode(valve1, OUTPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(pump2, OUTPUT);
+  pinMode(valve2, OUTPUT);
+  
+  digitalWrite(pump1, pump1_status);
+  digitalWrite(valve1, valve1_status);
+  digitalWrite(pump2, pump2_status);
+  digitalWrite(valve2, valve2_status);
+  
   digitalWrite(CLEANER_SWITCH, HIGH); 
-
   Serial.begin(9600);
 
-  
 }
 
 void loop()
 {
-    while(digitalRead(CLEANER_SWITCH) == HIGH) //check if cleaner switch is ON
+    while(digitalRead(CLEANER_SWITCH) == LOW) //check if cleaner switch is ON
     {
-        liquid_sensor_status_current = digitalRead(LIQUID_SENSOR); // read sensor status
-        Serial.println(liquid_sensor_status_current); 
-        if (liquid_sensor_status_previous == LOW && liquid_sensor_status_current == HIGH) // sensor trigger ON, needs liquid
+        int sensor1_reading = digitalRead(sensor1); // read sensor status
+        int sensor2_reading = digitalRead(sensor2);
+
+        pump1_status = digitalRead(pump1);
+        valve1_status = digitalRead(valve1);
+
+        Serial.print("Sensor1_reading: ");
+        Serial.println(sensor1_reading);
+        Serial.print("Pump1_status: ");
+        Serial.println(pump1_status);
+        Serial.print("Valve1_status: ");
+        Serial.println(valve1_status);
+
+        if (sensor1_reading !=  sensor1_status_last) //sensor state has changed.
             {
-            liquid_sensor_trigger_ON_timestamp = millis();
+            sensor1_last_changed_time = millis();
+            Serial.println("Sensor status changed!");
             }
-        if (liquid_sensor_status_previous == HIGH && liquid_sensor_status_current == LOW) // sensor trigger OFF, stop liquid
+
+        if (sensor2_reading !=  sensor2_status_last) //sensor state has changed.
             {
-            liquid_sensor_trigger_OFF_timestamp = millis();
-            }
-        if (liquid_sensor_trigger_ON != 0 && millis() - liquid_sensor_trigger_ON > 500) // start pump
+            sensor2_last_changed_time = millis();
+            Serial.println("Sensor status changed!");
+            }          
+            
+        if (millis() - sensor1_last_changed_time > debounceDelay) // A delay time has passed since last change
             {
-            digitalWrite(sol_pump_rel1, HIGH); // turn on pump
-            liquid_sensor_trigger_ON = 0;
+              if (sensor1_reading != sensor1_status) // this means the change of sensor status persists.
+                {
+                  sensor1_status = sensor1_reading;
+                  
+                  if (sensor1_status == HIGH) // seonsor low to high, need liquid, should turn on pump
+                  {
+                    pump1_status = HIGH;
+                   }
+                  else if (sensor1_status == LOW) // sensor high to low, liquid enough, should turn off pump
+                  {
+                    pump1_status = LOW;
+                   }
+                }
             }
-        if (liquid_sensor_trigger_OFF != 0 && millis() - liquid_sensor_trigger_OFF > 500) // stop pump
+
+        if (millis() - sensor2_last_changed_time > debounceDelay) // A delay time has passed since last change
             {
-            digitalWrite(sol_pump_rel1, LOW); // turn off pump
-            liquid_sensor_trigger_OFF_timestamp = 0;
+              if (sensor2_reading != sensor2_status) // this means the change of sensor status persists.
+                {
+                  sensor2_status = sensor2_reading;
+                  
+                  if (sensor2_status == HIGH) // seonsor low to high, need liquid, should turn on pump
+                  {
+                    pump2_status = HIGH;
+                   }
+                  else if (sensor2_status == LOW) // sensor high to low, liquid enough, should turn off pump
+                  {
+                    pump2_status = LOW;
+                   }
+                }
             }
-        liquid_sensor_status_previous = liquid_sensor_status_current;
+
+        digitalWrite(pump1, pump1_status);
+        digitalWrite(valve1, pump1_status);
+        digitalWrite(pump2, pump2_status);
+        digitalWrite(valve2, pump2_status);
+
+
+        sensor1_status_last = sensor1_reading;
+        sensor2_status_last = sensor2_reading;
+
         delay(10); // Add a small delay to prevent a tight loop
-
     }
-    // Ensure the pump is turned off when the cleaner switch is turned off
-    digitalWrite(sol_pump_rel1, LOW);
 
-    // Reset sensor states and times when the cleaner switch is turned off
-    liquid_sensor_status_current = 0;
-    liquid_sensor_status_previous = 0;
-    liquid_sensor_trigger_ON_timestamp = 0;
-    liquid_sensor_trigger_OFF_timestamp = 0;
-    // Add a small delay to prevent a tight loop
+    // Turn off all pumps and valves when the cleaner switch is turned off
+    digitalWrite(pump1, LOW);
+    digitalWrite(pump2, LOW);
+    digitalWrite(valve1, HIGH);
+    digitalWrite(valve2, LOW);
+
+    // Avoid tight loops
     delay(10);
 }
