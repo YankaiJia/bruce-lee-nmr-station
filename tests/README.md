c228dd59a9710f3d31d14bfe4d2b97e9792eb621
Yankai <jiayankaieric@gmail.com>
2025-02-18 15:24:49 +0900

    further clean up old files/scripts

--- tests/README.md
+++
@@ -1,2 +0,0 @@
-This folder is only for pytest tests, **not for anything else**.
-If a file is not used by pytest test, it does not belong here.
--- zeus-pipetter/obsolete_files/set_paras_to_zeus_from_brb.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import breadboard as brb
-import zeus
-
-zm = zeus.ZeusModule(id=1)
-time.sleep(2)
-
-zm.setContainerGeometryParameters(brb.vial_2ml)
