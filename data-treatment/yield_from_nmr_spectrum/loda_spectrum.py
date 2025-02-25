4a75f27e9ee00d3aa5da895fc00bd405d205d4c3
YankaiJia <72350068+YankaiJia@users.noreply.github.com>
2025-02-26 01:22:32 +0900

    loading data suceed

--- data-treatment/yield_from_nmr_spectrum/loda_spectrum.py
+++ data-treatment/yield_from_nmr_spectrum/loda_spectrum.py
@@ -4,7 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 # load nmr raw data from a spectrum
 spectrum = "C:/vscode_projects/brucelee/bruce-nmr-station/data-treatment/yield_from_nmr_spectrum"
-dic,data = ng.spinsolve.read(spectrum + '/acqu.par')
-# read csv file by numpy
-data = np.loadtxt(spectrum + '/data.csv', delimiter=',')
-print(data)
+dic,data = ng.spinsolve.read(spectrum)
+# plot the spectrum
+plt.plot(data)
+plt.show()
+# calculate the integral of the spectrum
+integral = np.sum(data)
+print("integral of the spectrum: ", integral)
+# calculate the yield of the spectrum
+yield_ = integral / 1000
+print("yield of the spectrum: ", yield_)
