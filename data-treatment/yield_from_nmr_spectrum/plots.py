e10c7bf0b67562cce6d806de1c04e3983e302e58
Yankai Jia <jiayankaieric@gmail.com>
2025-03-05 18:41:23 +0900

    add plots.py

---
+++ data-treatment/yield_from_nmr_spectrum/plots.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+
+
+ys_ori = {
+0:'92.03727024121508',
+1:'44.85000344605123',
+2:'21.30438829283912',
+3:'9.689694961722125',
+4:'4.404170234225603'}
+
+ys = [float(value) for key, value in ys_ori.items()]
+print(ys)
+
+xs_init = 152.4
+xs = [xs_init, xs_init/2, xs_init/4, xs_init/8, xs_init/16 ]
+
+yb_ori =  {0:'58.26061628299067',
+1:'29.7097988122041',
+2:'14.252173271263246',
+3:'6.550650316834435',
+4:'2.1064971663290635'}
+
+yb = [float(value) for key, value in yb_ori.items()]
+xb_init = 252.1
+xb = [xb_init,xb_init/2,xb_init/4,xb_init/8,xb_init/16 ]
+
+plt.plot(xs, ys,'o', ls = '-')
+plt.plot(xb, yb, 'o', ls = '-')
+
+# plt.xscale('log')  # Set the x-axis to logarithmic scale
+plt.plot()
+# plt.show()
+
+import pandas as pd
+# plot csv
+path ="D:\\Dropbox\\brucelee\\data\\DPE_bromination\\_Refs\\ref_B_TEST\\205244-1D EXTENDED+-B1\\data.csv"
+
+df = pd.read_csv(path)
